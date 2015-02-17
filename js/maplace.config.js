20a10fdcb938803f8c78424c94fafd5743cbf2e4
Elliott-Greaves <greaveselliott@me.com>
2015-02-17 20:46:51 +0000

    added 2 email templates, 1 thank you page and 1 sign up page

---
+++ js/maplace.config.js
@@ -0,0 +1,12 @@
+// maplace.js config file
+(function($){
+    new Maplace({
+        show_markers: false,
+        locations: [{
+            lat: 51.4973163,
+            lon: -0.1395884,
+            zoom: 15
+        }],
+        generate_controls: false
+    }).Load();
+})(jQuery);
---
+++ template-microsoft-lync.php
@@ -0,0 +1,234 @@
+<?php
+/*
+* Template Name: Microsoft Lync
+*/
+$rootDir = 'http://www.ipsolutions.co.uk/wp-content/themes/grandcollege/ip-solutions/'
+?>
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>IP Solutions: Microsoft Lync</title>
+    <link rel="stylesheet" href="<?php echo $rootDir; ?>stylesheets/app.css" />
+    <script src="<?php echo $rootDir; ?>bower_components/modernizr/modernizr.js"></script>
+  </head>
+  <body>
+    <!-- header / nav -->
+    <header class="row full-screen">
+        <div class="large-12 columns">
+            <!-- header -->
+            <div class="left">
+                <div id="logos" class="padding-top-10 margin-bottom-10">
+                    <img src="images/ipsolutionslogo.jpg">
+                    <img src="images/lync_logo_1.jpg">
+                    <img src="images/microsoft-logo.png">
+                </div>
+            </div>
+            <!-- nav -->
+            <!--<nav class="right ul-stick-bottom">-->
+                <!--<ul>-->
+                    <!--<li class="display-inline-block"><a href="#" class="link-agenda">Agenda</a></li>-->
+                    <!--<li class="display-inline-block"><a href="#" class="link-location">Location</a></li>-->
+                    <!--<li class="display-inline-block"><a href="#" class="link-contact">Contact</a></li>-->
+                <!--</ul>-->
+            <!--</nav>-->
+        </div>
+        <!-- background -->
+        <div class="ips-background ips-background-white"></div>
+    </header>
+    <!--
+        row 1: Is Microsoft Lync right for you?
+        - background image -->
+    <section class="full-screen padding-60-75">
+        <div class="row">
+            <div class="large-6 columns large-centered margin-bottom-50">
+                <h2 class="text-center ips-white">Ever wondered how to voice enable Office 365?</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="large-10 columns large-centered">
+                <!-- FORM START -->
+                <div class="row ips-background-box ips-background-blue" id="sign-up-form">
+                    <!-- Title -->
+                    <div class="large-6 columns">
+                        <h2 class="ips-white heading-line-height">Join us on at Microsoft in London on the 1st April to find out how you can maximise Office 365 functionality</h2>
+                        <hr>
+                        <h4 class="ips-white">Without voice enabling Office 365 you could be losing more than 20% functionality.</h4>
+                    </div>
+                    <!-- Sign up -->
+                    <div class="large-6 columns">
+                        <h3 class="ips-white padding-bottom-10">Sign up</h3>
+                        <script charset="utf-8" src="//js.hsforms.net/forms/current.js"></script>
+                        <script>
+                            hbspt.forms.create({
+                                portalId: '330853',
+                                formId: '2153ac64-29d3-4bae-84eb-c57dafd200fb'
+                            });
+                        </script>
+
+
+                        <!--                        <form>-->
+<!--                            <label><span class="hide">Name</span>-->
+<!--                                <input type="text" placeholder="Name" />-->
+<!--                            </label>-->
+<!--                            <label><span class="hide">Company</span>-->
+<!--                                <input type="text" placeholder="Company" />-->
+<!--                            </label>-->
+<!--                            <label><span class="hide">Job Title</span>-->
+<!--                                <input type="text" placeholder="Job Title" />-->
+<!--                            </label>-->
+<!--                            <label><span class="hide">Email</span>-->
+<!--                                <input type="text" placeholder="Email" />-->
+<!--                            </label>-->
+<!--                            <input type="submit" class="button ips-background-green">-->
+<!--                        </form>-->
+                    </div>
+                </div>
+                <!-- FORM END -->
+            </div>
+        </div>
+        <!-- Background -->
+        <div class="ips-background-image1"></div>
+        <div class="ips-background ips-background-grey"></div>
+    </section>
+    <!-- row 2: Agenda
+         - grey background
+    -->
+    <section class="full-screen padding-60-75 padding-top-0">
+        <div class="row">
+            <div class="large-7 columns large-centered">
+                <h2 class="text-upper-case text-center ips-white">Agenda</h2>
+                <table>
+                    <tr>
+                        <td class="text-bold">9:30am</td><td>Breakfast, refreshments and networking</td>
+                    </tr>
+                    <tr>
+                        <td class="text-bold">10:00am</td><td>Welcome and housekeeping – IP Solutions Raza Baloch & Andy Lindsell (leading into…)</td>
+                    </tr>
+                    <tr>
+                        <td class="text-bold">10:10am</td><td>IP Solutions Cloud Strategy – Raza Baloch & Andy Lindsell</td>
+                    </tr>
+                    <tr>
+                        <td class="text-bold">10:40am</td><td>The benefits of voice-enabling O365 – ( and a nod to Skype for Business ) Damon Crawford</td>
+                    </tr>
+                    <tr>
+                        <td class="text-bold">11:00am</td><td>Comfort break</td>
+                    </tr>
+                    <tr>
+                        <td class="text-bold">11:20am</td><td>Why Cloud and How to Get There – Vicky Ryder & IP Solutions</td>
+                    </tr>
+                    <tr>
+                        <td class="text-bold">11:40am</td><td>The Subscription Economy: Capex versus Opex – VR & IP Solutions</td>
+                    </tr>
+                    <tr>
+                        <td class="text-bold">12:00pm</td><td>Q&A</td>
+                    </tr>
+                    <tr>
+                        <td class="text-bold">12:15pm</td><td>Lunch and networking</td>
+                    </tr>
+                    <tr>
+                        <td class="text-bold">1:00pm</td><td>Close</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <!-- background -->
+        <div class="ips-background ips-background-grey"></div>
+    </section>
+    <!--
+        row 3: Location
+        - white background
+    -->
+    <section class="full-screen padding-60-75">
+        <div class="row">
+            <div class="large-4 columns large-centered">
+                <h2 class="text-upper-case text-center ips-grey">Location</h2>
+            </div>
+        </div>
+        <div class="row padding-60-75">
+            <div class="large-9 columns large-centered">
+                <div class="row">
+                    <div class="large-6 columns">
+                        <figure id="gmap">
+
+                        </figure>
+                    </div>
+                    <div class="large-6 columns">
+                        <ul class="ips-grey list-style-none">
+                            <li>Cardinal Place</li>
+                            <li>Auditorium1</li>
+                            <li>80-100 Victoria Street</li>
+                            <li>London</li>
+                            <li class="text-upper-case">sw1e 5jl</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- background -->
+        <div class="ips-background ips-background-white"></div>
+    </section>
+    <!--
+        row 4: Sign up today
+        - background image
+    -->
+    <section class="full-screen ips-section padding-60-75">
+        <div class="row">
+            <div class="large-10 columns large-centered">
+                <!-- FORM START -->
+                <div class="row ips-background-box ips-background-blue">
+                    <!-- Title -->
+                    <div class="large-6 columns">
+                        <div class="table-center-container large-min-height-320">
+                            <div class="table-center-cell">
+                                <h3 class="h1 text-upper-case ips-white text-left">Sign up today to attend today</h3>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- Sign up -->
+                    <div class="large-6 columns">
+                        <div class="table-center-container large-min-height-320">
+                            <div class="table-center-cell">
+                                <a href="#sign-up-form" class="ips-white button ips-background-green padding-60-75 h3">Sign up now</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- FORM END -->
+            </div>
+        </div>
+        <!-- background -->
+        <div class="ips-background ips-background-image2"></div>
+    </section>
+    <!--
+        row 5: Connect with us
+    -->
+    <section class="full-screen padding-10-30-0-10">
+        <div class="row">
+            <div class="large-12 columns text-center">
+                <h3 class="text-upper-case display-inline-block hide-for-small ips-white">Connect with us</h3>
+                <nav class="display-inline-block">
+                    <ul>
+                        <li class="display-inline-block"><a href="#" class="link-social-twitter ips-green h3"><i class="fa fa-twitter-square"></i></a></li>
+                        <li class="display-inline-block"><a href="#" class="link-social-linkedIn ips-green h3"><i class="fa fa-linkedin-square"></i></a></li>
+<!--                        <li class="display-inline-block"><a href="#" class="link-ip-solutions ips-green"><img src="images/ipsolutionslogo.jpg"></a></li>-->
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <!-- background -->
+        <div class="ips-background ips-background-grey"></div>
+    </section>
+
+    <script src="<?php echo $rootDir; ?>bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="<?php echo $rootDir; ?>bower_components/foundation/js/foundation.min.js"></script>
+    <script src="<?php echo $rootDir; ?>js/app.js"></script>
+    <script src="http://maps.google.com/maps/api/js?sensor=false&libraries=geometry&v=3.7">
+    </script>
+    <script src="<?php echo $rootDir; ?>js/maplace-0.1.3.js"></script>
+    <script src="<?php echo $rootDir; ?>js/maplace.config.js"></script>
+    </body>
+</html>
